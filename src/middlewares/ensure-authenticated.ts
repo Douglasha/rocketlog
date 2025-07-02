e7668873a3069162cfd38f9ef3f12024fde4181d
Douglasha <douglas.henriqueamaral@gmail.com>
2025-07-02 20:57:58 -0300

    Criando ensure-authenticated

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+import { env } from '../env'
+import { AppError } from '../utils/AppError'
+import { authConfig } from '@/configs/auth'
+
+interface ITokenPayload {
+  role: string
+  sub: string
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  
+  
+  
+  
+  try {
+    const authHeader = request.headers.authorization
+    
+    if (!authHeader) {
+      throw new AppError('JWT token is missing', 401)
+    }
+    
+    const [, token] = authHeader.split(' ')
+
+    const { role, sub: user_id } = verify(token, authConfig.jwt.secret) as ITokenPayload
+
+    request.user = {
+      id: user_id,
+      role,
+    }
+
+    return next()
+  } catch {
+    throw new AppError('Invalid JWT token', 401)
+  }
+}
