b3526328aa6d2fe04a2f685cf206996ae187dc92
Douglasha <douglas.henriqueamaral@gmail.com>
2025-07-02 21:07:24 -0300

    Cria Deliveries Controller e Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express"
 
 import { usersRoutes } from "./users-routes.js"
 import { sessionsRoutes } from "./sessions-routes.js"
+import { deliveriesRoutes } from "./deliveries-routes.js"
 
 const routes = Router()
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
+routes.use("/deliveries", deliveriesRoutes)
 
 export { routes }
