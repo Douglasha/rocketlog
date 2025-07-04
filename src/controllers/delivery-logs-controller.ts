e63c454784e6ba6febbbd265e79a9b0367a40a01
Douglasha <douglas.henriqueamaral@gmail.com>
2025-07-04 20:07:41 -0300

    Cadastra Log

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,8 +1,38 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
+import { AppError } from '@/utils/AppError'
+import { prisma } from '@/database/prisma'
+import { z } from 'zod'
 
 class DeliveryLogsController {
   async create(request: Request, response: Response) {
-        return response.json({message: "ok"});
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string().min(1)
+    })
+
+    const { delivery_id, description } = bodySchema.parse(request.body)
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id }
+    })
+
+    if (!delivery) {
+      throw new AppError("Delivery not found", 404)
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("Change status to shipped", 400)
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description
+      }
+    })
+
+
+        return response.status(201).json()
   }
 }
 export { DeliveryLogsController }
